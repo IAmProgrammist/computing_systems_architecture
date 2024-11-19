009eea8d2c49892d841f54f418d62f73e7c30e23
IAmProgrammist <vladislav.pakhomov@bk.ru>
2024-11-19 21:31:22 +0300

    Deleted unused files, updated gitignore

--- lab5/prot.bat
+++
@@ -1,8 +0,0 @@
-set app_name=%~n0
-del %app_name%.exe
-set masm32_path=C:\masm32
-%masm32_path%\bin\ml /c /coff /I "%masm32_path%\include" %app_name%.asm
-%masm32_path%\bin\link /SUBSYSTEM:CONSOLE /LIBPATH:%masm32_path%\lib %app_name%.obj
-pause
-%app_name%.exe
-pause
