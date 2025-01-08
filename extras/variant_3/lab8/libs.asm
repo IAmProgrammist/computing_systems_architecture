b0359347f3e3d35b6c46d670da9905a71a0f2711
IAmProgrammist <vladislav.pakhomov@bk.ru>
2025-01-09 02:05:33 +0300

    WIP

--- extras/variant_3/lab8/libs.asm
+++ extras/variant_3/lab8/libs.asm
@@ -14,64 +14,166 @@ mov eax, 1
 ret
 DllMain endp
 
-; sort_inner(float* a, int length)
-sort_inner proc
+; select_sort(int* a, int length)
+select_sort proc
     pushad
+    mov    ebp, esp
+    mov    ecx, [ebp + 32 + 4 + 4] ; Количество чисел
+    mov    ebx, [ebp + 32 + 4] ; Адрес числа
 
-    mov ecx, 0
-sort_inner_loop:
-    
+    sub    ecx, 1       ; Готово если 0 или 1 элемент
+    jbe    select_sort_end
 
+select_sort_outer_loop:
+        mov    edx, ecx      ; Количество сравнений
+        mov    esi, ebx      ; Начало неотсортированного массива
+        mov    eax, [esi]    ; Минимум (число)
+        mov    edi, esi      ; Минимум (адрес)
+select_sort_inner_loop:
+            add    esi, 4
+
+            cmp [esi], eax
+
+            jg     select_sort_not_smaller
+            mov    eax, [esi]    ; Значение минимума
+            mov    edi, esi      ; Адрес минимума
+select_sort_not_smaller:
+            dec    edx
+            jnz    select_sort_inner_loop
+
+            mov    edx, [ebx]    ; Обмен элементов
+            mov    [ebx], eax
+            mov    [edi], edx
+
+            add    ebx, 4       ; Передвинуть границу отсортированного массива
+            dec    ecx
+            jnz    select_sort_outer_loop
+select_sort_end:
     popad
-    ret 8
-sort_inner endp
+    ret    8
+select_sort endp
 
 
-; int sort_stdcall_noarg (float* a, int length, float* pos_res, float* neg_res, int* pos_count)
+; int* sort_stdcall_noarg(int* a, int start, int end, int* res)
 sort_stdcall_noarg proc
     push ebp
-    mov ecx, [esp + 8]       ; ecx = a
-    mov edx, [esp + 8 + 4]   ; edx = length
+    push edi
+    push esi
+    push ebx
 
-    ; pos_count = 0
-    mov edi, dword ptr [esp + 8 + 16]
-    mov dword ptr [edi], 0
+    mov ecx, [esp + 20]       ; ecx = a
+    mov edx, [esp + 20 + 4]   ; edx = start
+    mov edi, [esp + 20 + 8]   ; edi = end
+    mov ebx, [esp + 20 + 12]  ; ebx = res 
 
-    ; Используемые аргументы
+    mov eax, 0
 
-    ; ebx будет нашим счётчиком
-    mov ebx, 0
+    ; Копируем данные в res
+sort_stdcall_noarg_copyloop:
+    ; start > end?
+    cmp edx, edi
+    jg sort_stdcall_noarg_copyloop_end
+    
+    ; ebp = res + eax * 4
+    mov ebp, ebx
+    add ebp, eax
+    add ebp, eax
+    add ebp, eax
+    add ebp, eax
 
+    push edi
+    push eax
+    ; edi = a + start * 4
+    mov edi, ecx
+    add edi, edx
+    add edi, edx
+    add edi, edx
+    add edi, edx
 
+    ; res[eax] = a[start]
+    mov eax, dword ptr [edi]
+    mov dword ptr [ebp], eax
+    pop eax
+    pop edi
 
+    ; start++
+    ; eax++
+    inc eax
+    inc edx
 
+    jmp sort_stdcall_noarg_copyloop
+sort_stdcall_noarg_copyloop_end:
 
+    push eax
+    push ebx
+    call select_sort
+
+    mov eax, ebx
+    pop ebx
+    pop esi
+    pop edi
     pop ebp
-    ret 5 * 4
+    ret 4 * 4
 sort_stdcall_noarg endp
 
-; int sort_cdecl_noarg (int* a, int length, int* pos_res, int* neg_res, int* neg_count)
+; int sort_cdecl_noarg (int* a, int start, int end, int* res)
 sort_cdecl_noarg proc
     push ebp
-    mov ecx, [esp + 8]       ; ecx = a
-    mov edx, [esp + 8 + 4]   ; edx = length
+    push edi
+    push esi
+    push ebx
 
-    ; neg_count = 0
-    mov edi, dword ptr [esp + 8 + 16]
-    mov dword ptr [edi], 0
+    mov ecx, [esp + 20]       ; ecx = a
+    mov edx, [esp + 20 + 4]   ; edx = start
+    mov edi, [esp + 20 + 8]   ; edi = end
+    mov ebx, [esp + 20 + 12]  ; ebx = res 
 
-    ; Используемые аргументы
+    mov eax, 0
 
-    ; Выделяем место под локальную переменную current, j, current_comparing
-    sub esp, 12
-    ; ebx будет нашим счётчиком
-    mov ebx, 0
-    ; Обнуление eax
-    xor eax, eax
+    ; Копируем данные в res
+sort_stdcall_noarg_copyloop:
+    ; start > end?
+    cmp edx, edi
+    jg sort_stdcall_noarg_copyloop_end
+    
+    ; ebp = res + eax * 4
+    mov ebp, ebx
+    add ebp, eax
+    add ebp, eax
+    add ebp, eax
+    add ebp, eax
 
+    push edi
+    push eax
+    ; edi = a + start * 4
+    mov edi, ecx
+    add edi, edx
+    add edi, edx
+    add edi, edx
+    add edi, edx
 
+    ; res[eax] = a[start]
+    mov eax, dword ptr [edi]
+    mov dword ptr [ebp], eax
+    pop eax
+    pop edi
 
-    add esp, 12
+    ; start++
+    ; eax++
+    inc eax
+    inc edx
+
+    jmp sort_stdcall_noarg_copyloop
+sort_stdcall_noarg_copyloop_end:
+
+    push eax
+    push ebx
+    call select_sort
+
+    mov eax, ebx
+    pop ebx
+    pop esi
+    pop edi
     pop ebp
     ret
 sort_cdecl_noarg endp
@@ -99,7 +201,7 @@ sort_fastcall_noarg proc
 
     add esp, 12
     pop ebp
-    ret 3 * 4
+    ret 2 * 4
 sort_fastcall_noarg endp
 
 ; int sort_stdcall (int* a, int length, int* pos_res, int* neg_res, int* neg_count)
--- extras/variant_3/lab8/libs.cpp
+++ extras/variant_3/lab8/libs.cpp
@@ -5,195 +5,169 @@
 
 #pragma comment(lib, "libs.lib")
 
-extern "C" __declspec(dllimport) int _stdcall  sort_stdcall_noarg  (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
-extern "C" __declspec(dllimport) int _cdecl    sort_cdecl_noarg    (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
-extern "C" __declspec(dllimport) int _fastcall sort_fastcall_noarg (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
+extern "C" __declspec(dllimport) int* _stdcall  sort_stdcall_noarg  (int* a, int start, int end, int* res);
+extern "C" __declspec(dllimport) int* _cdecl    sort_cdecl_noarg    (int* a, int start, int end, int* res);
+/*extern "C" __declspec(dllimport) int _fastcall sort_fastcall_noarg (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
 extern "C" __declspec(dllimport) int _stdcall  sort_stdcall        (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
-extern "C" __declspec(dllimport) int _cdecl    sort_cdecl          (int* a, int length, int* pos_res, int* neg_res, int* neg_count);
+extern "C" __declspec(dllimport) int _cdecl    sort_cdecl          (int* a, int length, int* pos_res, int* neg_res, int* neg_count);*/
 
-int sort_native(int* a, int length, int* pos_res, int* neg_res, int* neg_count) {
-	int pos_count = 0;
-	*neg_count = 0;
-	
-	for (int i = 0; i < length; i++) {
-		if (a[i] > 0) {
-			int j = pos_count;
-			while (j > 0 && pos_res[j - 1] > a[i]) {
-				pos_res[j] = pos_res[j - 1];
-				j--;
-			}
-
-			pos_res[j] = a[i];
-			pos_count++;
-		} else {
-			int j = *neg_count;
-			while (j > 0 && neg_res[j - 1] > a[i]) {
-				neg_res[j] = neg_res[j - 1];
-				j--;
-			}
-
-			neg_res[j] = a[i];
-			(*neg_count)++;
-		}
+int* sort_native(int* a, int start, int end, int* res) {
+	int i = 0;
+	int length = 0;
+	while (start <= end) {
+		res[i++] = a[start++];
+		length++;
 	}
 
-	return pos_count;
+	if (length <= 1) return res;
+
+	for (i = 0; i < length - 1; i++) {
+		int min_element = res[i];
+		int min_element_index = i;
+
+		for (int j = i + 1; j < length; j++) {
+			if (res[j] < min_element) {
+				min_element_index = j;
+				min_element = res[j];
+			}
+		}
+
+		int tmp = min_element;
+		res[min_element_index] = res[i];
+		res[i] = min_element;
+	}
+
+	return res;
 }
 
 template <typename TestedFunction>
 void test_function1(TestedFunction func_to_test) {
 	int a[] = { -1, -2, -3, -4, -5, -6 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 0);
-	assert(neg_count == 6);
+	assert(res == sort_res);
 	assert(
-		neg_res[0] == -6 &&
-		neg_res[1] == -5 &&
-		neg_res[2] == -4 &&
-		neg_res[3] == -3 &&
-		neg_res[4] == -2 &&
-		neg_res[5] == -1);
+		sort_res[0] == -6 &&
+		sort_res[1] == -5 &&
+		sort_res[2] == -4 &&
+		sort_res[3] == -3 &&
+		sort_res[4] == -2 &&
+		sort_res[5] == -1);
 }
 
 template <typename TestedFunction>
 void test_function2(TestedFunction func_to_test) {
 	int a[] = { 6, 5, 4, 3, 2, 1 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[4] = {};
+	auto res = func_to_test(a, 1, 4, sort_res);
 
-	assert(pos_count == 6);
-	assert(neg_count == 0);
+	assert(res == sort_res);
 	assert(
-		pos_res[0] == 1 &&
-		pos_res[1] == 2 &&
-		pos_res[2] == 3 &&
-		pos_res[3] == 4 &&
-		pos_res[4] == 5 &&
-		pos_res[5] == 6);
+		sort_res[0] == 2 &&
+		sort_res[1] == 3 &&
+		sort_res[2] == 4 &&
+		sort_res[3] == 5);
 }
 
 template <typename TestedFunction>
 void test_function3(TestedFunction func_to_test) {
 	int a[] = { -1, 2, -3, 3, 45, -6 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 3);
-	assert(neg_count == 3);
+	assert(res == sort_res);
 	assert(
-		neg_res[0] == -6 &&
-		neg_res[1] == -3 &&
-		neg_res[2] == -1 &&
-		pos_res[0] == 2 &&
-		pos_res[1] == 3 &&
-		pos_res[2] == 45);
+		sort_res[0] == -6 &&
+		sort_res[1] == -3 &&
+		sort_res[2] == -1 &&
+		sort_res[3] == 2 &&
+		sort_res[4] == 3 &&
+		sort_res[5] == 45);
 }
 
 template <typename TestedFunction>
 void test_function4(TestedFunction func_to_test) {
 	int a[] = { -6, -5, -4, -3, -2, -1 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 0);
-	assert(neg_count == 6);
+	assert(res == sort_res);
 	assert(
-		neg_res[0] == -6 &&
-		neg_res[1] == -5 &&
-		neg_res[2] == -4 &&
-		neg_res[3] == -3 &&
-		neg_res[4] == -2 &&
-		neg_res[5] == -1);
+		sort_res[0] == -6 &&
+		sort_res[1] == -5 &&
+		sort_res[2] == -4 &&
+		sort_res[3] == -3 &&
+		sort_res[4] == -2 &&
+		sort_res[5] == -1);
 }
 
 template <typename TestedFunction>
 void test_function5(TestedFunction func_to_test) {
 	int a[] = { 1, 2, 3, 4, 5, 6 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 6);
-	assert(neg_count == 0);
+	assert(res == sort_res);
 	assert(
-		pos_res[0] == 1 &&
-		pos_res[1] == 2 &&
-		pos_res[2] == 3 &&
-		pos_res[3] == 4 &&
-		pos_res[4] == 5 &&
-		pos_res[5] == 6);
+		sort_res[0] == 1 &&
+		sort_res[1] == 2 &&
+		sort_res[2] == 3 &&
+		sort_res[3] == 4 &&
+		sort_res[4] == 5 &&
+		sort_res[5] == 6);
 }
 
 template <typename TestedFunction>
 void test_function6(TestedFunction func_to_test) {
 	int a[] = { 6, 3, 4, 2, 1, 5 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 6);
-	assert(neg_count == 0);
+	assert(res == sort_res);
 	assert(
-		pos_res[0] == 1 &&
-		pos_res[1] == 2 &&
-		pos_res[2] == 3 &&
-		pos_res[3] == 4 &&
-		pos_res[4] == 5 &&
-		pos_res[5] == 6);
+		sort_res[0] == 1 &&
+		sort_res[1] == 2 &&
+		sort_res[2] == 3 &&
+		sort_res[3] == 4 &&
+		sort_res[4] == 5 &&
+		sort_res[5] == 6);
 }
 
 template <typename TestedFunction>
 void test_function7(TestedFunction func_to_test) {
 	int a[] = { -4, -2, -6, -1, -5, -3 };
-	int pos_res[6] = {};
-	int neg_res[6] = {};
-	int neg_count;
-	auto pos_count = func_to_test(a, 6, pos_res, neg_res, (int*)(&neg_count));
+	int sort_res[6] = {};
+	auto res = func_to_test(a, 0, 5, sort_res);
 
-	assert(pos_count == 0);
-	assert(neg_count == 6);
+	assert(res == sort_res);
 	assert(
-		neg_res[0] == -6 &&
-		neg_res[1] == -5 &&
-		neg_res[2] == -4 &&
-		neg_res[3] == -3 &&
-		neg_res[4] == -2 &&
-		neg_res[5] == -1);
+		sort_res[0] == -6 &&
+		sort_res[1] == -5 &&
+		sort_res[2] == -4 &&
+		sort_res[3] == -3 &&
+		sort_res[4] == -2 &&
+		sort_res[5] == -1);
 }
 
 template <typename TestedFunction>
 void stress_test(TestedFunction func_to_test, int amount) {
 	srand(0);
 	int *a = (int*)malloc(sizeof(int) * amount);
-	int *pos_res = (int*)malloc(sizeof(int) * amount);
-	int *neg_res = (int*)malloc(sizeof(int) * amount);
-	int neg_count;
+	int *sort_res = (int*)malloc(sizeof(int) * amount);
 	for (int i = 0; i < amount; i++) {
 		a[i] = rand() % 1000;
 	}
 
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	auto pos_count = func_to_test(a, amount, pos_res, neg_res, (int*)(&neg_count));
+	auto res = func_to_test(a, 0, amount - 1, sort_res);
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
 
 	std::cout << "Working time: " << delta / 1000.0 << std::endl;
 
 	free(a);
-	free(pos_res);
-	free(neg_res);
+	free(sort_res);
 }
 
 template <typename TestedFunction>
@@ -213,16 +187,18 @@ void test_function(TestedFunction func_to_test) {
 int main() {
 	std::cout << "Native function:" << std::endl;
 	test_function(sort_native);
-	std::cout << "__cdecl auto parameters:" << std::endl;
+	/*std::cout << "__cdecl auto parameters:" << std::endl;
 	test_function(sort_cdecl);
 	std::cout << "__stdcall auto parameters:" << std::endl;
 	test_function(sort_stdcall);
 	std::cout << "__fastcall manual parameters:" << std::endl;
-	test_function(sort_fastcall_noarg);
+	test_function(sort_fastcall_noarg);*/
 	std::cout << "__stdcall manual parameters" << std::endl;
 	test_function(sort_stdcall_noarg);
 	std::cout << "__cdecl manual parameters" << std::endl;
 	test_function(sort_cdecl_noarg);
+
+	
 
 	return 0;
 }
